7ad76f7b4359dd823442a1611a5d963bec157285
Doeun <lde28700409@gmail.com>
2025-11-11 10:08:11 +0900

    postman

--- src/main/java/doeun/store/springbootdeveloper/TestController.java
+++ src/main/java/doeun/store/springbootdeveloper/TestController.java
@@ -1,4 +1,10 @@
 package doeun.store.springbootdeveloper;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello, World!";
+    }
 }
