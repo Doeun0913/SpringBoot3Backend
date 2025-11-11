15655c4539aedd21fdb4056270246a89dafddebc
Doeun <lde28700409@gmail.com>
2025-11-11 16:42:22 +0900

    로그인&회원가입

---
+++ src/main/java/doeun/store/springbootdeveloper/service/UserDetailService.java
@@ -0,0 +1,20 @@
+package doeun.store.springbootdeveloper.service;
+
+import doeun.store.springbootdeveloper.domain.User;
+import doeun.store.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserDetailService implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User loadUserByUsername(String email) {
+        return userRepository.findByEmail(email)
+                .orElseThrow(() -> new IllegalArgumentException((email)));
+    }
+
+}
---
+++ src/main/java/doeun/store/springbootdeveloper/service/UserService.java
@@ -0,0 +1,22 @@
+package doeun.store.springbootdeveloper.service;
+
+import doeun.store.springbootdeveloper.domain.User;
+import doeun.store.springbootdeveloper.dto.AddUserRequest;
+import doeun.store.springbootdeveloper.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+
+    public Long save(AddUserRequest dto) {
+        return userRepository.save(User.builder()
+                .email(dto.getEmail())
+                .password(bCryptPasswordEncoder.encode(dto.getPassword()))
+                .build()).getId();
+    }
+}
